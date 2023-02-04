7eafcedee25dbd91357512f5edb08305d6212820
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-04 17:37:36 +0900

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Circle c = new Circle();
-        System.out.println(c);
-
-        Circle c2 = new Circle(2.3);
-        System.out.println(c2);
-    }
-}
