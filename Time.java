80e863fe71a67d143bf8ee2bd544441dccd7253d
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-04 20:34:32 +0900

    added constructor

--- Time.java
+++ Time.java
@@ -2,4 +2,10 @@ public class Time {
     private int hour;
     private int minute;
     private int second;
+
+    public Time(int h, int m, int s) {
+        this.hour = h;
+        this.minute = m;
+        this.second = s;
+    }
 }
