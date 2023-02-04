0d89fe049a026b65193289e2b7b2efc324bc8648
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-04 18:49:11 +0900

    added Main class for Time.java

---
+++ TimeMain.java
@@ -0,0 +1,13 @@
+public class TimeMain {
+    public static void main(String[] args) {
+        Time t = new Time(15, 42, 33);
+        System.out.println(t);
+
+        for (int i = 0; i < 100; i++) {
+            t.nextSecond();
+        }
+        // t.nextSecond().nextSecond();
+        System.out.println(t);
+        
+    }
+}
