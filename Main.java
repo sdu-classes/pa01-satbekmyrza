1e6aeff20ba69925055bd7a64f22ed3b615ad40a
Satbek Abdyldayev <satbekmyrza@gmail.com>
2023-02-04 16:50:25 +0900

    added Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        System.out.println(c);
+
+        Circle c2 = new Circle(2.3);
+        System.out.println(c2);
+    }
+}
